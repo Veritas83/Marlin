a5dd22d2dc57729920372c61c61f746e2c50eede
Nigel Todman <veritas@vts-tech.org>
2020-10-03 15:42:06 -0400

    R18 - Invert extruder flow

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "VTSTECH MKS_GEN_L_V2 ANET_A8 CONFIG_R17" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "VTSTECH MKS_GEN_L_V2 ANET_A8 CONFIG_R18" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -1084,7 +1084,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
